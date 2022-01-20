a86b5fc64873cf0e7ce0363b8c3c3caee9ca4eab
IAGanja <abichaika@yandex.ru>
2022-01-20 14:51:13 +0300

    Test 1 - ArrayList Test 2 - HashSet Test 3 - ArrayList Test 4 - HashMap

--- src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
+++ src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
@@ -2,8 +2,7 @@ package com.sbrf.reboot.collections;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,9 +29,16 @@ public class CollectionsTest {
     @Test
     public void addStudentToRating() {
 
-        List<String> students = null;
+        // Наеврное в формулировке задания предполгася связанный список, раз студент
+        // доавлялся в начало списка, но можно предпологать, что в будущем студенты будут
+        // добавляться и в середену списка тоже. Так что ArrayList.
+        List<String> students = new ArrayList<>();
 
-        //...
+        students.add("Козлов");
+        students.add("Петров");
+        students.add("Сидоров");
+        students.add("Ганжа");
+
 
         assertEquals(4, students.size());
     }
@@ -50,9 +56,18 @@ public class CollectionsTest {
     @Test
     public void addMoneyToBox() {
 
-        Set<Integer> moneyBox = null;
+        Set<Integer> moneyBox = new HashSet<>();
 
-        //...
+        moneyBox.add(1);
+        moneyBox.add(2);
+        moneyBox.add(3);
+        moneyBox.add(4);
+        moneyBox.add(5);
+        moneyBox.add(6);
+        moneyBox.add(7);
+        moneyBox.add(8);
+        moneyBox.add(9);
+        moneyBox.add(10);
 
         assertEquals(10, moneyBox.size());
     }
@@ -72,11 +87,51 @@ public class CollectionsTest {
         class Book {
         }
 
-        List<Book> bookshelf = null;
+        List<Book> bookshelf = new ArrayList<>();
 
-        //...
+        bookshelf.add(new Book());
+        bookshelf.add(new Book());
+        bookshelf.add(new Book());
 
         assertEquals(3, bookshelf.size());
+    }
+
+    /*
+     * Задание на 5+.
+     *
+     * Придумать и реализовать хотя бы один свой пример использования Collection framework
+     * с описанием аналогично существующим.
+     *
+     * Задача.
+     * Имеется список студентов вуза.
+     *
+     * 1. Иванов
+     * 2. Петров
+     * 3. Сидоров
+     *
+     * В каждом семестре происходит подсчетов оценок студент.
+     *
+     * Вопрос.
+     * Какую коллекцию из Collection framework вы предпочтете для хранения и
+     * использования списка студентов и их результатво?
+     *
+     * Предпологается, что фамилии студентов УНИКАЛЬНЫ!
+     *
+     * Проинициализируйте students, добавьте в нее 4 фамилии студентов с их суммарной оценкой
+     *  что бы тест завершился успешно.
+     */
+    @Test
+    public void addStudentWithRating() {
+
+        Map<String, Integer> students = new HashMap<>();
+
+        students.put("Козлов",55);
+        students.put("Петров",54);
+        students.put("Сидоров",53);
+        students.put("Ганжа",52);
+
+
+        assertEquals(4, students.size());
     }
 
 
