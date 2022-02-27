db242b62b02c5a73486abc6fab53c4153d432d5b
IAGanja <abichaika@yandex.ru>
2022-02-27 17:20:06 +0300

    Реализовал отправку отчета используя CompletableFuture. Добился успешного выполнения CompletableFeatureTest.

--- src/main/java/com/sbrf/reboot/service/SomeService.java
+++ src/main/java/com/sbrf/reboot/service/SomeService.java
@@ -21,10 +21,12 @@ public class SomeService {
             public String call() throws Exception {
 
                 // Реализуйте отправку отчета используя CompletableFuture
-                String reportResult = reportService.sendReport("Отправляю отчет");
+                CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> reportService.sendReport("Отправляю отчет"));
 
                 //какой то код..
                 Thread.sleep(Duration.ofSeconds(3).toMillis());
+
+                String reportResult = cf.get();
 
                 if (reportResult.equals("SUCCESS")) {
                     System.out.println("Отчет отправлен успешно");
